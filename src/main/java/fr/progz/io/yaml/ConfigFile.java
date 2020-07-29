35a5ab15baab1cfbffe5a34bf0053199bba03d16
Meltwin <meltwin158@gmail.com>
2020-07-29 14:37:07 +0200

    Fixed some issues on ConfigFile + added possibility to put a null default file

--- src/main/java/fr/progz/io/yaml/ConfigFile.java
+++ src/main/java/fr/progz/io/yaml/ConfigFile.java
@@ -7,8 +7,8 @@ import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 public class ConfigFile {
 
@@ -27,37 +27,33 @@ public class ConfigFile {
     /**
      * @param c the getClass() of the calling class
      * @param dir the path of the config dir of the plugin (from the dir of the server.jar)
-     * @param name the path of the out file without the extension (ex "test" will resut in ./dir/test.yml)
+     * @param name the path of the out file without the extension (ex "test" will resut in ./configDir/innerDir/test.yml)
      * @param defaultFile the path of the default file within the jar 'ressources/defaultFile' (without the extension)
      */
-    public ConfigFile(Class<? extends Object> c, String dir, String outFile, String defaultFile) {
+    public ConfigFile(Class<? extends Object> c, String configDir, String innerDir,String outFile, String defaultFile) {
         // Creating the File instance
         _class = c;
         _defaultFile = defaultFile;
-        _dir = (dir.substring(dir.length() - 1).equals("/")) ? dir : dir + "/";
+        _dir = (configDir.substring(configDir.length() - 1).equals("/")) ? configDir : configDir + "/";
+        _dir = _dir + ((innerDir.substring(innerDir.length() - 1).equals("/")) ? innerDir : innerDir + "/");
         _fileName = String.format("%s.yml", outFile);
         _file = new File(String.format("%1$s%2$s", _dir, _fileName));
         checkExist();
-        importDefault();
-        //_config = YamlConfiguration.loadConfiguration(_file);
+        _config = YamlConfiguration.loadConfiguration(_file);
     }
 
     private void checkExist() {
         if (!_file.exists()) {
             System.out.println(String.format("File %1$s%2$s not found, creating it",_dir,_fileName));
             _file.getParentFile().mkdirs();
-            importDefault();
+            if (_defaultFile != null) importDefault();
         }
     }
 
     private void importDefault() {
         try {
-            // Read
-            InputStream in = _class.getResourceAsStream(String.format("/%s.yml",_defaultFile));
-            Files.copy(in,Paths.get(_file.getAbsolutePath()));
-        } 
-        catch (FileAlreadyExistsException f) {
-            return;
+            InputStream in = _class.getResourceAsStream(String.format("/%s.yml",_defaultFile)); // Read
+            Files.copy(in,Paths.get(_file.getAbsolutePath())); // Write
         }
         catch (IOException e) {
             e.printStackTrace();
