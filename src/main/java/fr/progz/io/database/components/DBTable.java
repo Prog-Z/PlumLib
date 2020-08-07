dddc071dc1a3e41b661489bf6437ecf2a57a912d
Meltwin <meltwin158@gmail.com>
2020-08-07 18:55:03 +0200

    [DB] Added option of creating unique constraints in dbtable

--- src/main/java/fr/progz/io/database/components/DBTable.java
+++ src/main/java/fr/progz/io/database/components/DBTable.java
@@ -3,18 +3,19 @@ package fr.progz.io.database.components;
 import java.util.ArrayList;
 
 import fr.progz.io.database.components.columns.DBColumn;
+import fr.progz.plumlib.container.Pair;
 
 public abstract class DBTable {
     protected ArrayList<DBColumn> columns = new ArrayList<>();
     protected ArrayList<String> lines = new ArrayList<>();
+    protected ArrayList<Pair<String>> uniques = new ArrayList<>();
     protected boolean exist = false;
 
     // #####################################################
     // SECTION Table Making
     // #####################################################
-    protected void appendColumn(DBColumn column) {
-        columns.add(column);
-    }
+    protected void appendColumn(DBColumn column) { columns.add(column); }
+    protected void addUnique(String colA, String colB) { uniques.add(new Pair<String>(colA,colB)); }
     public abstract String getTableName();
     // !SECTION
 
@@ -23,14 +24,17 @@ public abstract class DBTable {
     // #####################################################
     public String makeRequest() {
 
-        String output = String.format("CREATE TABLE %s(",getTableName());
+        String output = String.format("CREATE TABLE %s(",getTableName()); // INIT
+
+        // ADD Column
         boolean first = true;
         for(DBColumn col : columns) {
             if (!first) output += ", ";
             else first = false;
-
             output += col.makeRequest();
         }
+        // ADD Uniques
+        for (Pair<String> u : uniques) output += ", UNIQUE("+u.get(false)+","+u.get(true)+") ";
         output += ")";
 
         return output;
